bcc91f034c00e554b93249ea3ce4a352c0a06851
vamsigalanki <vamsigalinki@gmail.com>
2024-10-30 14:00:40 +0530

    class component

---
+++ src/Acomponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+class Acomponent extends React.Component{
+    render (){
+        return (
+            <div>
+                <h1>welcome to react</h1>
+                <h4>Manth tech</h4>
+            </div>
+        ) 
+        
+           
+              
+           
+            
+            
+    }
+}
+export default Acomponent;
