c35d33c6ac6df124b87cdc8e0e96ccbad4b7afc4
vamsigalanki <vamsigalinki@gmail.com>
2024-11-13 13:27:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
 import './App.css';
+import Lifecycle from './Lifecycle';
 
 function App() {
+  var [count,setCount]=React.useState(356)
   return (
     <div className="App">
-      
+      <Lifecycle></Lifecycle>
     </div>
   );
 }
--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,1 +1,8 @@
-var class
+ import React from "react"
+ class Lifecycle extends React.Component {
+    render (){
+        return(<div>life cycling method</div>)
+     }
+ }
+ export default Lifecycle
+ 
